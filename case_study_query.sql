5e27d8a2abfa497db0669d615051898d8d31b605
hien1171 <xyzwn12345@gmail.com>
2023-05-26 18:14:20 +0700

    bt

--- case_study_query.sql
+++ case_study_query.sql
@@ -1,7 +1,8 @@
 USE FuramaManagement;
 -- 2.Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự. 
 SELECT * FROM nhan_vien
-where ho_ten like 'H%' or ho_ten like  'T%' or ho_ten like 'K%';
+where ho_ten like 'H%' or ho_ten like  'T%' or ho_ten like 'K%'AND length(ho_ten) <= 15;
+;
 -- 3.	Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
 SELECT * FROM khach_hang
 WHERE YEAR(CURRENT_DATE) - YEAR(ngay_sinh) BETWEEN 18 and 50
@@ -14,7 +15,47 @@ JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
 WHERE lk.ten_loai_khach = 'Diamond'
 GROUP BY kh.ho_ten;
 -- 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
-SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+SELECT 
+    khach_hang.ma_khach_hang,
+    khach_hang.ho_ten,
+    loai_khach.ten_loai_khach,
+    hop_dong.ma_hop_dong,
+    dich_vu.ten_dich_vu,
+    hop_dong.ngay_lam_hop_dong,
+    hop_dong.ngay_ket_thuc,
+    (hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia + dich_vu.chi_phi_thue) AS tong_tien
+FROM
+    khach_hang
+    LEFT JOIN loai_khach ON khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+    LEFT JOIN hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+    LEFT JOIN hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+    LEFT JOIN dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+    LEFT JOIN dich_vu_di_kem ON hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+ORDER BY khach_hang.ma_khach_hang;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 SELECT kh.ma_khach_hang, kh.ho_ten, kh.ho_ten, hd.ma_hop_dong, dv.ten_dich_vu, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, IFNULL(t.totalCPT + IFNULL(SUM(hdct.so_luong * dvdk.gia), 0),0) total FROM khach_hang kh
 LEFT JOIN hop_dong hd USING(ma_khach_hang)
 LEFT JOIN hop_dong_chi_tiet hdct USING(ma_hop_dong)
@@ -28,6 +69,28 @@ LEFT JOIN dich_vu dv USING(ma_dich_vu)
 GROUP BY kh.ma_khach_hang
 ) t USING(ma_hop_dong)
 GROUP BY kh.ma_khach_hang ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 -- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
 
 SELECT
